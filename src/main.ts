a9706884ea1cd43ff84a2cf21182d606998c4881
blackorion <104806177+Mr-Binod@users.noreply.github.com>
2025-07-28 02:59:12 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
     origin: 'https://bing-nft.vercel.app',
     credentials: true,
   })
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
