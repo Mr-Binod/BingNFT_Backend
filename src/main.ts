37570efc10e6390ac5e90af05ba616ccf0825b18
blackorion <104806177+Mr-Binod@users.noreply.github.com>
2025-07-28 02:58:46 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    // origin: 'http://localhost:3000',
+    // origin: '*',
+    origin: 'https://bing-nft.vercel.app',
     credentials: true,
   })
   await app.listen(process.env.PORT ?? 3001);
