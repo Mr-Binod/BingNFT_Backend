e1da4e9ae6e33449bbd521faa22fffeeb1242bf5
Mr-Binod <blackorion3@gmail.com>
2025-07-28 14:38:58 +0900

    origin changed

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    // origin: 'http://localhost:3000',
+    origin: `bing-nft.vercel.app`,
     credentials: true,
   })
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
