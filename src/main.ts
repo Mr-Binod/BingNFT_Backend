946baf98056779990a6109cbd959738138eabf88
blackorion <104806177+Mr-Binod@users.noreply.github.com>
2025-07-29 22:22:09 +0900

    dist file origin changed

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: `https://bing-nft.vercel.app/`,
+    origin: `https://bing-nft.vercel.app`,
     // origin: '*',
     credentials: true,
   })
